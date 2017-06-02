8a4952b7afc8289b219cd09fe722eaeda1a85bae
Shrish Apte <shrish.apte@gmail.com>
2017-06-02 13:30:59 -0400

    Table View Setup

---
+++ Pods/Target Support Files/Pods-TelstraTests/Pods-TelstraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TelstraTests : NSObject
+@end
+@implementation PodsDummy_Pods_TelstraTests
+@end
--- Telstra/ViewController.m
+++ Telstra/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource, NSURLSessionDelegate>
+
+@property (strong,nonatomic) UITableView *tableView;
+@property (strong,nonatomic) NSMutableArray *content;
+
+
 
 @end
 
@@ -16,12 +22,103 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self cofigureTableview];
+    
+    
+    self.tableView.estimatedRowHeight = 20;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+   
+    self.content = [[NSMutableArray alloc]init];
+    
+     [self fetchData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)cofigureTableview
+{
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
 }
+
+
+//MARK: Tableview Datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+//    if (self.content.count > 0) {
+//     return   _content.count ;
+//    }
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cellIdentifier";
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if(cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        
+    }
+
+    if (self.content.count > 0){
+    NSDictionary *contentdata = self.content[indexPath.row];
+        
+        NSString *title = contentdata[@"title"];
+        NSString *imageUrlString = contentdata[@"imageHref"];
+        
+        cell.textLabel.text =  [title isKindOfClass:[NSNull class]] ? @"NO Data" : title;
+        
+       
+
+    }
+    
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    cell.textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+   // NSLog(@"title of cell %@", [_content objectAtIndex:indexPath.row]);
+}
+
+
+
+//---- api call --
+-(void)fetchData{
+
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"]];
+    [request setHTTPMethod:@"GET"];
+    [request addValue:@"text/plain" forHTTPHeaderField:@"Content-Type"];
+    [request addValue:@"text/plain" forHTTPHeaderField:@"Accept"];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    [[session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *requestReply = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+        NSData * responseData = [requestReply dataUsingEncoding:NSUTF8StringEncoding];
+        if (responseData != nil) {
+        
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+            NSLog(@"requestReply: %@", jsonDict);
+            self.content = jsonDict[@"rows"];
+            [self.tableView reloadData];
+            
+            NSLog(@"result %@", self.content);
+        
+        }
+        
+    }] resume];
+    
+    
+}
+
+
 
 @end
